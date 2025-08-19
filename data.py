37f1f737c3e9f59d741e959a4028055e3384d548
Esau Salvador Reyes Tellez <sal.reyes05@gmail.com>
2025-08-18 21:26:49 -0600

    git commit

---
+++ data.py
@@ -0,0 +1,10 @@
+header = {
+    "Content-Type": "application/json"
+}
+
+create_user = {
+    "firstName": "Max",
+    "phone": "+10005553535",
+    "address": "8042 Lancaster Ave.Hamburg, NY"
+}
+
