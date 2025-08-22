a04480585bc465cd3573b275f4f51505fcb3a244
Esau Salvador Reyes Tellez <sal.reyes05@gmail.com>
2025-08-21 20:36:22 -0600

    commit change

--- data.py
+++ data.py
@@ -8,3 +8,38 @@ create_user = {
     "address": "8042 Lancaster Ave.Hamburg, NY"
 }
 
+kit_name = {
+    "name": "kit",
+}
+
+kit_body = {
+    "name": "a",
+}
+kit_body_2 = {
+    "name": "AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC",
+}
+
+kit_body_3 = {
+    "name": "",
+}
+kit_body_4 = {
+    "name": "AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD",
+}
+kit_body_5 = {
+    "name": "N@%%"
+}
+
+kit_body_6 = {
+    "name": "A Aaa",
+}
+kit_body_7 = {
+    "name": "123",
+}
+kit_body_8 = {
+    "name":{}
+}
+kit_body_9 = {
+    "name":{123}
+}
+
+
